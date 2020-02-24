91ce4a0fdeb7044657d9e66922de0fac79a81ac6
Gregory Anderson <anderson.gregory.mail@gmail.com>
2020-02-24 10:45:37 -0700

    Initial React commit

---
+++ src/Main/index.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import { Switch, Route, BrowserRouter as Router } from 'react-router-dom'
+import { Security, ImplicitCallback, SecureRoute } from '@okta/okta-react';
+
+import Login from '../Login'
+import Home from '../Home'
+
+const config = {
+  issuer: 'https://dev-969790.okta.com/oauth2/default',
+  redirectUri: window.location.origin + '/implicit/callback',
+  clientId: '{0oa2hxqx0wQ13B7rX4x6}',
+  pkce: true
+}
+
+
+class Main extends Component {
+
+ render() {
+   return (
+     <Router>
+       <Security
+         issuer={'https://dev-969790.okta.com/oauth2/default'}
+         client_id={'0oa2hxqx0wQ13B7rX4x6'}
+         redirect_uri={'http://localhost:8081/implicit/callback'}
+         scope={['openid', 'profile', 'email']}>
+        
+         <Switch>
+           <Route exact path="/" component={Login} />
+           <Route path="/implicit/callback" component={ImplicitCallback} />
+           <SecureRoute path="/home" component={Home} />
+         </Switch>
+       </Security>
+     </Router>
+   );
+ }
+}
+
+export default Main;
